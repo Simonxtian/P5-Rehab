140e94c12786defc456527fecb065ec9258d395d
viktoraau <cj7.viktor@gmail.com>
2025-11-28 15:31:52 +0100

    FORCE_EMA_ALPHA  = 1 and potmeter calibrated

--- WristRehab/Control.h
+++ WristRehab/Control.h
@@ -61,10 +61,10 @@ public:
       float theta_pot_rad = adcToThetaRad(adc);
       //float theta_pot_deg = theta_pot_rad * RAD_TO_DEG;
       float theta_pot_deg = fabs(theta_pot_rad * RAD_TO_DEG);
-      Serial.print(theta_pot_deg -90);   // angle in degrees
+      Serial.print(theta_pot_deg);   // angle in degrees
       Serial.print(',');
       Serial.println(digitalRead(11));  // button state: 0 or 1
-      // Serial.print(theta_pot_rad);           Serial.print(',');
+      // Serial.println(theta_pot_rad);           //Serial.print(',');
       // Serial.println(digitalRead(11));  // Serial.print(',');
       // Serial.print(theta_enc, 6);        Serial.print(',');
       // Serial.print(wUser_, 6);           Serial.print(',');
--- WristRehab/config.h
+++ WristRehab/config.h
@@ -36,7 +36,7 @@ static constexpr float THETA_MIN_DEG = 0.0f;
 static constexpr float THETA_MAX_DEG = 270.0f;
 static constexpr float THETA_MIN_RAD = THETA_MIN_DEG * DEG_TO_RAD;
 static constexpr float THETA_MAX_RAD = THETA_MAX_DEG * DEG_TO_RAD;
-static constexpr float POT_OFFSET_RAD =  -4.1600f;   // align to your home
+static constexpr float POT_OFFSET_RAD =  -1.99f;   // align to your home
 
 // ----------------- Gravity comp -----------------
 static constexpr float DELTA_OFFSET_RAD = 0.0f;   // phase offset if needed
@@ -46,7 +46,7 @@ static constexpr float DELTA_OFFSET_RAD = 0.0f;   // phase offset if needed
 // ----------------- Filters & timing -----------------
 static constexpr unsigned long SPEED_WIN_US = 1000;  // speed oversampling window
 static constexpr uint8_t W_MED_WIN = 3;               // median length (1,3,5,…)
-static constexpr float FORCE_EMA_ALPHA = 0.8f;        // LPF for force
+static constexpr float FORCE_EMA_ALPHA = 1.0f;        // LPF for force
 
 static constexpr float Omega_EMA_ALPHA = 1.0f;
 
