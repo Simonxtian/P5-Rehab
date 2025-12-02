d95b389fab9c32168a2c465117ba1a058a624377
viktoraau <cj7.viktor@gmail.com>
2025-12-02 11:41:46 +0100

    Removed emaAlpha

--- WristRehab/Utils.h
+++ WristRehab/Utils.h
@@ -5,12 +5,6 @@ inline float saturate(float x, float lo, float hi){
   if (x < lo) return lo; if (x > hi) return hi; return x;
 }
 
-inline float emaAlpha(float fc_hz, float dt_s) {
-  if (fc_hz <= 0.0f) return 1.0f; // bypass -> snap
-  float tau = 1.0f / (2.0f * PI * fc_hz);
-  return dt_s / (dt_s + tau);
-}
-
 inline float emaStep(float prev, float x, float alpha) {
   return prev + alpha * (x - prev);
 }
